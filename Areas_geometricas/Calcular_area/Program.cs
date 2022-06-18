e9910d7a22b8a7ab88329fab02a779538d2bbc1e
Daury <daurymendoza@hotmail.com>
2022-06-17 21:26:34 -0400

    Se agrego el proyecto

---
+++ Areas_geometricas/Calcular_area/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calcular_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este es el proyecto de mi solución
+            //Daury Mendoza 2021-1645
+        }
+    }
+}
